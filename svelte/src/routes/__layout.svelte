0f6a47aa8642edd3644b0ebc34ab67b77cb62743
WORAKRIT SOONTORNTHAMNITI <worakrit.soo@pea.co.th>
2021-08-07 14:11:52 +0700

    d

---
+++ svelte/src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import '../app.postcss';
+</script>
+
+<header>
+  <nav>
+    <ul>
+      <a href="/">Home</a>
+    </ul>
+  </nav>
+</header>
+<slot />
--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,2 +1,11 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev" target="_blank">kit.svelte.dev</a> to read
+  the documentation
+</p>
+
+<fieldset>
+  <legend> Fieldset </legend>
+  <label for="title">Title</label>
+  <input type="text" name="title" id="title" placeholder="title" />
+</fieldset>
